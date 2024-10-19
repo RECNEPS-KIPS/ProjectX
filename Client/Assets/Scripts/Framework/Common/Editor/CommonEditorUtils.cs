059f92c07fa0a336f86f95478445ae75538258b6
KIPKIPS <2432719656@qq.com>
2024-10-19 17:14:08 +0800

    主流程和Lobby场景

---
+++ Client/Assets/Scripts/Framework/Common/Editor/CommonEditorUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using Application = UnityEngine.Application;
+
+namespace Framework.Common
+{
+    public static class CommonEditorUtils
+    {
+        [MenuItem("Tools/GameLaunch",false,-9999999)]
+        private static void GameLaunch()
+        {
+            const string path = "Assets/ResourcesAssets/Scenes/Launch.unity";
+            var sceneName = Path.GetFileNameWithoutExtension(path);
+            var bIsCurScene = SceneManager.GetActiveScene().name.Equals(sceneName);//是否为当前场景
+            EditorApplication.ExecuteMenuItem("Edit/Play");
+            if (!bIsCurScene)
+            {
+                EditorSceneManager.OpenScene(path);
+            }
+            EditorApplication.ExecuteMenuItem("Edit/Play");
+        }
+    }
+    
+}
